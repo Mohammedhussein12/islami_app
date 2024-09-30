a925a1432dc2be61cb65f704a4be195b4377244c
mohammed hussein <muhammedhussein739@gmail.com>
2024-09-30 15:31:47 +0300

    initialize project

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const IslamiApp());
+}
+
+class IslamiApp extends StatelessWidget {
+  const IslamiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
