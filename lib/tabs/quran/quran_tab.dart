2368fa3764c602c5d3fd1d61e75ab4799c35ba2f
mohammed hussein <muhammedhussein739@gmail.com>
2024-10-03 22:19:11 +0300

    finished quran tab and quran item in list view

--- lib/tabs/quran/quran_tab.dart
+++ lib/tabs/quran/quran_tab.dart
@@ -1,14 +1,102 @@
 import 'package:flutter/material.dart';
+import 'package:islami/controllers/sura_controller.dart';
+import 'package:islami/tabs/quran/quran_item.dart';
+import 'package:islami/tabs/quran/sura_content_screen.dart';
 
 class QuranTab extends StatelessWidget {
   const QuranTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Quran Tab'),
+    final SuraController suraController = SuraController();
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Column(
+        children: [
+          Image.asset(
+            "assets/images/qur2an_screen_logo.png",
+            height: height * 0.25,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              border:
+                  Border.all(color: Theme.of(context).primaryColor, width: 3),
+            ),
+            width: double.infinity,
+            height: height * 0.1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(
+                  child: Text(
+                    textAlign: TextAlign.center,
+                    'عدد الآيات',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineSmall!
+                        .copyWith(fontWeight: FontWeight.w600),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: width * 0.05),
+                  child: const VerticalDivider(),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: Text(
+                    textAlign: TextAlign.center,
+                    'إسم السورة',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineSmall!
+                        .copyWith(fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: suraController.suraNames.length,
+              itemBuilder: (context, index) => IntrinsicHeight(
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: QuranItem(
+                            item: suraController.versesNumber[index].toString(),
+                            index: index)),
+                    const VerticalDivider(),
+                    Expanded(
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).pushNamed(
+                            SuraContentScreen.routeName,
+                            arguments: SuraDetailsArgs(
+                                suraName: suraController.suraNames[index],
+                                index: index),
+                          );
+                        },
+                        child: QuranItem(
+                          item: suraController.suraNames[index],
+                          index: index,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+}
+
+class SuraDetailsArgs {
+  String suraName;
+  int index;
+
+  SuraDetailsArgs({required this.suraName, required this.index});
 }
