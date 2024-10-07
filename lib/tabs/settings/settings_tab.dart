2a7c7c54eb6ee1963becf23829a78bb83c7ea651
mohammed hussein <muhammedhussein739@gmail.com>
2024-10-07 23:52:54 +0300

    finished design of settings tab.

--- lib/tabs/settings/settings_tab.dart
+++ lib/tabs/settings/settings_tab.dart
@@ -1,13 +1,81 @@
 import 'package:flutter/material.dart';
 
-class SettingsTab extends StatelessWidget {
+import 'language.dart';
+
+class SettingsTab extends StatefulWidget {
   const SettingsTab({super.key});
 
   @override
+  State<SettingsTab> createState() => _SettingsTabState();
+}
+
+class _SettingsTabState extends State<SettingsTab> {
+  List<Language> languages = [
+    Language(name: 'English', code: 'en'),
+    Language(name: 'العربيه', code: 'ar'),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Settings Tab'),
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.symmetric(
+            horizontal: width * 0.05, vertical: height * 0.08),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Dark Theme',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleLarge!
+                      .copyWith(fontWeight: FontWeight.w500),
+                ),
+                Switch(
+                  value: true,
+                  onChanged: (value) {},
+                ),
+              ],
+            ),
+            SizedBox(
+              height: height * 0.02,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Language',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleLarge!
+                      .copyWith(fontWeight: FontWeight.w500),
+                ),
+                DropdownButtonHideUnderline(
+                  child: DropdownButton<Language>(
+                    borderRadius: BorderRadius.circular(20),
+                    value: languages.first,
+                    items: languages.map((language) {
+                      return DropdownMenuItem(
+                        value: language,
+                        child: Text(language.name),
+                      );
+                    }).toList(),
+                    onChanged: (selsctedLanguage) {
+                      if (selsctedLanguage != null) {
+                        print(selsctedLanguage.code);
+                      }
+                    },
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
